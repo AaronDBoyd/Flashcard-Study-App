687d70a8a50dccacb62feba6911489a7910a288d
Aaron Boyd <A.boyd91@yahoo.com>
2023-11-17 20:24:46 -0800

    signup user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages & components
-import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
 import Category from "./pages/Category";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   const { user } = useAuthContext();
@@ -22,7 +23,7 @@ function App() {
               path="/login"
               element={!user ? <Login /> : <Navigate to="/" />}
             />
-            {/* <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />} /> */}
+            <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />} /> 
           </Routes>
         </div>
       </BrowserRouter>
