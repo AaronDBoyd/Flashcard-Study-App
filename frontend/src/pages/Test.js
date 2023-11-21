fc5a4b026abfa806fc61aca2efe471f1d093df48
Aaron Boyd <A.boyd91@yahoo.com>
2023-11-21 01:13:38 -0800

    submit test answer and show correct or incorrect

--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -1,72 +1,129 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useCardContext } from "../hooks/useCardContext";
 import { shuffleArray } from "../helpers/shuffleArray";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import ModalBody from "react-bootstrap/esm/ModalBody";
+import { useNavigate } from "react-router-dom";
 
 const Test = () => {
-  const { cards } = useCardContext();
-  const [testAnswer, setTestAnswer] = useState('');
-  const [testCard, setTestCard] = useState(null)
-  const [showAnswer, setShowAnswer] = useState(false);
+	const { cards } = useCardContext();
+	const [testInput, setTestInput] = useState("");
+	const [testCard, setTestCard] = useState(null);
+	const [showAnswer, setShowAnswer] = useState(false);
+	const [totalCards, setTotalCards] = useState(0);
+	const [currentCardNumber, setCurrentCardNumber] = useState(1);
+    const [isCorrect, setIsCorrect] = useState(false)
 
-  
-  useEffect(() => {
+	const navigate = useNavigate();
 
-     shuffleArray(cards);
-    setTestCard(cards[0])    
-  }, [cards]);
+	useEffect(() => {
+		shuffleArray(cards);
+		setTestCard(cards[0]);
+		setTotalCards(cards.length);
+	}, [cards]);
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setShowAnswer(true);
+        setTestInput('')
+        if (testCard.answer.toLowerCase() === testInput.toLowerCase()){
+            setIsCorrect(true)
+        }
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setShowAnswer(true);
-};
+	const handleNext = () => {
+		cards.shift();
+		setTestCard(cards[0]);
+		setShowAnswer(false);
+		setCurrentCardNumber((prev) => prev + 1);
+        setIsCorrect(false)
+	};
 
-const handleNext = () => {
-    
-    cards.shift()
-    
-    setTestCard(cards[0])
-    setShowAnswer(false)
- 
-  };
+	const handleHome = () => {
+		navigate("/");
+	};
 
-  //const handleShow = () => setShow(true);
+	return (
+		<div>
+			<div className="test">
+				<div className="text-end">
+					<h4>
+						Card {currentCardNumber} of {totalCards}
+					</h4>
+				</div>
+				<div className="test-question">
+					<h2>{testCard && testCard.question}</h2>
+				</div>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group className="mb-3">
+						<Form.Label>Answer</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="type answer"
+							onChange={(e) => setTestInput(e.target.value)}
+							value={testInput}
+						/>
+					</Form.Group>
+					<Button variant="info" type="submit">
+						Submit
+					</Button>
+				</Form>
+			</div>
+			<Modal
+				show={showAnswer}
+				className="test-answer-modal"
+				size="lg"
+				centered="true"
+			>
+				<ModalBody className="text-center my-5">
+					<h3>{testCard && testCard.answer}</h3>
+				</ModalBody>
+				<Modal.Footer>
+					<Container>
+						<Row>
+							<Col xs={9}>
+								<div>
+									{testCard &&
+										isCorrect ? (
+											<h3>Correct!</h3>
+										) : (
+											<h3>Incorrect</h3>
+										) }
 
-  return (
-    <div className="container">
-           
-      <div>
-        <h2>{testCard && testCard.question}</h2>
-      </div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Answer</Form.Label>
-          <Form.Control type="text" placeholder="type answer" 
-          onChange={(e) => setTestAnswer(e.target.value)}
-          value={testAnswer}/>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-      <Modal show={showAnswer}>
-        {testCard && testCard.answer}
-        <Button variant="primary" onClick={handleNext} disabled={cards.length <= 1}>
-          Next
-        </Button>
-      {testCard && testCard.answer === testAnswer && 
-      <h3>Correct</h3>}
-
-{testCard && testCard.answer !== testAnswer && 
-      <h3>Inorrect</h3>}
-      </Modal>
-    
-    </div>
-  );
+								</div>
+							</Col>
+							<Col xs={3}>
+								{currentCardNumber < totalCards ? (
+									<Button
+										className="next-button"
+										variant="info"
+										onClick={handleNext}
+										
+									>
+										Next
+									</Button>
+								) : (
+									<Button
+										className="next-button"
+										variant="outline-info"
+										onClick={handleHome}
+										
+									>
+										 Go Home
+									</Button>
+								)}
+							</Col>
+						</Row>
+					</Container>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 };
 
 export default Test;
